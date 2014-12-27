ced6de9de217c7c3dc6df5b9214882fb2b4cb374
ppkt <github@ppkt.eu>
2014-12-27 16:58:00 +0100

    added initialization of stm32f1 Discovery LEDs and button

--- utils.c
+++ utils.c
@@ -13,3 +13,33 @@ void delay_decrement(void)
 {
   if (_delay--) {}
 }
+
+// Init Discovery LEDs
+void LED_Init() {
+	/* Enable GPIO clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+	// Use PC8 and PC9 // Discovery LEDs
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+// Initialize Discovery User Button
+void BTN_Init() {
+
+	/* Enable GPIO clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	// Use PC8 and PC9 // Discovery LEDs
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
--- utils.h
+++ utils.h
@@ -1,8 +1,12 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
-#include "stm32f10x.h"
+#include <stm32f10x.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_rcc.h>
 
-void delay(__IO uint32_t nTime);
+void delay(__IO uint32_t nTime /*ms*/);
 void delay_decrement(void);
+void LED_Init();
+void BTN_Init();
 
 #endif
