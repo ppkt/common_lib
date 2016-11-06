4b4ad57df721a6c53263976d6d5eecd28d38fb18
Karol Werner <karol@ppkt.eu>
2016-11-06 16:54:32 +0100

    1Wire: Refactor library

--- one_wire.h
+++ one_wire.h
@@ -1,34 +1,37 @@
 #ifndef __ONE_WIRE_H__
 #define __ONE_WIRE_H__
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
 #include <stm32f10x.h>
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_tim.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdio.h>
-#include "usart.h"
+
+#include "common_lib/usart.h"
+#include "common_lib/utils.h"
 
 typedef enum {
-	ONE_WIRE_SLAVE_PRESENT,
-	ONE_WIRE_ERROR
+    ONE_WIRE_SLAVE_PRESENT,
+    ONE_WIRE_ERROR
 } one_wire_state;
 
 typedef struct {
-	u8 address[8];
+    uint8_t address[8];
 } one_wire_device;
 
-void one_wire_init(GPIO_TypeDef *gpio, u16 port, TIM_TypeDef *timer);
-bool one_wire_reset_pulse();
-void one_wire_write_1();
-void one_wire_write_0();
+void one_wire_init(GPIO_TypeDef *gpio, uint16_t port, TIM_TypeDef *timer);
+bool one_wire_reset_pulse(void);
+void one_wire_write_1(void);
+void one_wire_write_0(void);
 void one_wire_write_bit(bool bit);
-bool one_wire_read_bit();
-void one_wire_write_byte(u8 data);
-u8 one_wire_read_byte();
-one_wire_device * one_wire_search_rom(u8 *devices);
+bool one_wire_read_bit(void);
+void one_wire_write_byte(uint8_t data);
+uint8_t one_wire_read_byte(void);
+one_wire_device * one_wire_search_rom(uint8_t *devices);
 bool one_wire_match_rom(one_wire_device device);
-u8 one_wire_get_crc();
-u8 one_wire_crc(u8 data);
-void one_wire_reset_crc();
+uint8_t one_wire_get_crc(void);
+uint8_t one_wire_crc(uint8_t data);
+void one_wire_reset_crc(void);
 #endif // __ONE_WIRE_H__
