f9896e260c10edf4ee0a5ede950d078801746c5f
Karol Werner <karol@ppkt.eu>
2018-05-20 13:23:16 +0200

    USART: Add macros to print variable values

--- usart.h
+++ usart.h
@@ -21,6 +21,8 @@ void usart_printf(USART_TypeDef* usart, const char *format, ...);
 
 #define usart1_print(string) usart_print(USART1,string)
 #define usart1_printf(format,...) usart_printf(USART1,format,__VA_ARGS__)
+#define print_variable_int(variable) usart_printf(USART1,"%s = %d\r\n",#variable,variable)
+#define print_variable_float(variable, precision) usart_printf(USART1,"%s = %.*f\r\n",#variable,precision,variable)
 
 void usart_string_append(char c);
 void usart_clear_string();
