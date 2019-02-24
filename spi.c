1598009c2457fdf98a657e425a6997d59018186c
Karol Werner <karol@ppkt.eu>
2019-02-24 16:05:56 +0100

    SPI: Update to use libopencm3

--- spi.c
+++ spi.c
@@ -1,86 +1,93 @@
 #include "spi.h"
 
-void rcc_configuration(SPI_TypeDef *spi) {
+void
+spi_init(uint32_t spi) {
     if (spi == SPI1) {
-        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_SPI1, ENABLE);
+        rcc_periph_clock_enable(RCC_SPI1);
+        rcc_periph_clock_enable(RCC_GPIOA);
+
+        // Configure GPIOs: SS=PA4, SCK=PA5, MISO=PA6 and MOSI=PA7
+        gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+                      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                      GPIO_SPI1_SCK | GPIO_SPI1_MOSI);
+        gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+                      GPIO_CNF_OUTPUT_PUSHPULL,
+                      GPIO_SPI1_NSS);
+        gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
+                      GPIO_CNF_INPUT_FLOAT,
+                      GPIO_SPI1_MISO);
     } else if (spi == SPI2) {
-        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-        RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+        rcc_periph_clock_enable(RCC_SPI2);
+        rcc_periph_clock_enable(RCC_GPIOB);
+
+        // Configure GPIOs: SS=PB12, SCK=PB13, MISO=PB14 and MOSI=PB15
+        gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+                      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                      GPIO_SPI2_SCK | GPIO_SPI2_MOSI);
+        gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+                      GPIO_CNF_OUTPUT_PUSHPULL,
+                      GPIO_SPI2_NSS);
+        gpio_set_mode(GPIOB, GPIO_MODE_INPUT,
+                      GPIO_CNF_INPUT_FLOAT,
+                      GPIO_SPI2_MISO);
+    } else {
+        hacf();
     }
+
+    // reset and shut down peripheral
+    spi_reset(spi);
+
+    // Set up SPI in Master mode with:
+    // - Clock baud rate: 1/64 of peripheral clock frequency
+    // - Clock polarity: Idle Low
+    // - Clock phase: Data valid on leading edge
+    // - Data frame format: 8-bit
+    // - Frame format: MSB First
+    spi_init_master(spi,
+                    SPI_CR1_BAUDRATE_FPCLK_DIV_64,
+                    SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+                    SPI_CR1_CPHA_CLK_TRANSITION_1,
+                    SPI_CR1_DFF_8BIT,
+                    SPI_CR1_MSBFIRST
+    );
+
+    spi_enable_software_slave_management(spi);
+    spi_set_nss_high(spi);
+
+    spi_enable(spi);
+    spi_nss(spi, true);
 }
 
-void spi_gpio_configuration(SPI_TypeDef *spi) {
-    GPIO_InitTypeDef GPIO_InitStructure;
+void
+spi_nss(uint32_t spi, bool new_state) {
+    void (*ptr)(uint32_t, uint16_t);
 
+    if (new_state) {
+        ptr = &gpio_set;
+    } else {
+        ptr = &gpio_clear;
+    }
+
+    // TODO(ppkt): SS pin / bank should be bound to device, not peripheral!
     if (spi == SPI1) {
-        // Configure SPI1 pins: SCK, MISO and MOSI
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_7;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-        GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-        GPIO_Init(GPIOA, &GPIO_InitStructure);
+        ptr(GPIO_BANK_SPI1_NSS, GPIO_SPI1_NSS);
     } else if (spi == SPI2) {
-        // Configure SPI2 pins: SCK, MISO and MOSI
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_15;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-        GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-        GPIO_Init(GPIOB, &GPIO_InitStructure);
+        ptr(GPIO_BANK_SPI2_NSS, GPIO_SPI2_NSS);
+    } else {
+        hacf();
     }
 }
 
-void spi_init(SPI_TypeDef *spi) {
-    /* System clocks configuration ---------------------------------------------*/
-    rcc_configuration(spi);
-
-    /* GPIO configuration ------------------------------------------------------*/
-    spi_gpio_configuration(spi);
-
-    /* SPI Config -------------------------------------------------------------*/
-    SPI_I2S_DeInit(spi);
-    SPI_InitTypeDef   SPI_InitStructure;
-    SPI_InitStructure.SPI_Direction = SPI_Direction_Tx;
-    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-    SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
-    SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_128;
-    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-    SPI_InitStructure.SPI_CRCPolynomial = 7;
-    SPI_Init(spi, &SPI_InitStructure);
-
-    /* Enable SPI */
-    SPI_Cmd(spi, ENABLE);
-}
-
-void spi_send(SPI_TypeDef *spi, u8* tx, u8* rx, u8 size) {
-    u8 index;
-    for (index = 0; index < size; ++index) {
-        while (SPI_I2S_GetFlagStatus(spi, SPI_I2S_FLAG_TXE) == RESET);
-        SPI_I2S_SendData(spi, tx[index]);
-
-        while (SPI_I2S_GetFlagStatus(spi, SPI_I2S_FLAG_RXNE) == RESET);
-        rx[index] = SPI_I2S_ReceiveData(spi);
+void
+spi_send_transaction(uint32_t spi, uint8_t *tx, uint8_t *rx, uint8_t size) {
+    // TODO(ppkt): Add receiving data from SPI!
+    (void)rx;
+    spi_nss(spi, false);
+    for (uint8_t i = 0; i < size; ++i) {
+        spi_send(spi, tx[i]);
+        SPI_WAIT_FOR_TX_EMPTY(spi);
+        SPI_WAIT_FOR_BUSY_CLEAR(spi);
     }
-    // Wait for end of transmission
-    while (SPI_I2S_GetFlagStatus(spi, SPI_I2S_FLAG_TXE) == RESET);
-}
+    spi_nss(spi, true);
 
-void spi_nss(SPI_TypeDef *spi, bool new_state) {
-    BitAction action = Bit_RESET;
-    if (new_state)
-        action = Bit_SET;
-
-    if (spi == SPI1) {
-        GPIO_WriteBit(GPIOA, GPIO_Pin_4, action);
-    } else if (spi == SPI2) {
-        GPIO_WriteBit(GPIOB, GPIO_Pin_12, action);
-    }
 }
--- spi.h
+++ spi.h
@@ -1,15 +1,35 @@
-#ifndef __SPI_H___
-#define __SPI_H___
+#pragma once
 
-#include <stdbool.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/spi.h>
 
-#include "stm32f10x_dma.h"
-#include "stm32f10x_spi.h"
-#include "stm32f10x_rcc.h"
-#include "stm32f10x_gpio.h"
+#include "utils.h"
 
-void spi_init(SPI_TypeDef *spi);
-void spi_send(SPI_TypeDef *spi, u8* tx, u8* rx, u8 size);
-void spi_nss(SPI_TypeDef *spi, bool new_state);
+#define SPI_WAIT_FOR_TX_EMPTY(spi) while (!(SPI_SR(spi) & SPI_SR_TXE))
+#define SPI_WAIT_FOR_BUSY_CLEAR(spi) while ((SPI_SR(spi) & SPI_SR_BSY));
 
-#endif // __SPI_H__
+typedef struct spi_device {
+    uint32_t spi;
+} spi_device;
+
+
+/**
+ * Initialize SPI peripheral (including RCC and GPIOs)
+ */
+void
+spi_init(uint32_t spi);
+
+/**
+ * Send / receive data to SPI slave using transaction (i.e. pull down SS before
+ * 1st byte and pull up after all data is sent).
+ */
+void
+spi_send_transaction(uint32_t spi, uint8_t *tx, uint8_t *rx, uint8_t size);
+
+/**
+ * Set new value for SS depending on selected peripheral
+ */
+void
+spi_nss(uint32_t spi, bool new_state);
+
