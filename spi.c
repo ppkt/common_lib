bccf4aa1c4226cd578ff96bfc9c2d647355e07de
Karol Werner <karol@ppkt.eu>
2019-05-08 21:07:42 +0200

    SPI: Small refactoring
    * add support for STM32F0
    * format file using Clang Format
    * remove support for initializing / controlling NSS pin (it's
    responsibility of library)

--- spi.c
+++ spi.c
@@ -1,93 +1,71 @@
 #include "spi.h"
 
-void
-spi_init(uint32_t spi) {
-    if (spi == SPI1) {
-        rcc_periph_clock_enable(RCC_SPI1);
-        rcc_periph_clock_enable(RCC_GPIOA);
+void spi_init(uint32_t spi) {
+  if (spi == SPI1) {
+    rcc_periph_clock_enable(RCC_SPI1);
+    rcc_periph_clock_enable(RCC_GPIOA);
 
-        // Configure GPIOs: SS=PA4, SCK=PA5, MISO=PA6 and MOSI=PA7
-        gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-                      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-                      GPIO_SPI1_SCK | GPIO_SPI1_MOSI);
-        gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-                      GPIO_CNF_OUTPUT_PUSHPULL,
-                      GPIO_SPI1_NSS);
-        gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
-                      GPIO_CNF_INPUT_FLOAT,
-                      GPIO_SPI1_MISO);
-    } else if (spi == SPI2) {
-        rcc_periph_clock_enable(RCC_SPI2);
-        rcc_periph_clock_enable(RCC_GPIOB);
+    // Configure GPIOs: SCK=PA5, MISO=PA6 and MOSI=PA7
+#ifdef STM32F0
+    gpio_mode_setup(GPIO_BANK_SPI1, GPIO_MODE_AF, GPIO_PUPD_NONE,
+                    GPIO_SPI1_SCK | GPIO_SPI1_MOSI);
+    gpio_mode_setup(GPIO_BANK_SPI1, GPIO_MODE_AF, GPIO_PUPD_PULLUP,
+                    GPIO_SPI1_MISO);
+    gpio_set_af(GPIO_BANK_SPI1, GPIO_AF0,
+                GPIO_SPI1_SCK | GPIO_SPI1_MOSI | GPIO_SPI1_MISO);
 
-        // Configure GPIOs: SS=PB12, SCK=PB13, MISO=PB14 and MOSI=PB15
-        gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-                      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-                      GPIO_SPI2_SCK | GPIO_SPI2_MOSI);
-        gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-                      GPIO_CNF_OUTPUT_PUSHPULL,
-                      GPIO_SPI2_NSS);
-        gpio_set_mode(GPIOB, GPIO_MODE_INPUT,
-                      GPIO_CNF_INPUT_FLOAT,
-                      GPIO_SPI2_MISO);
-    } else {
-        hacf();
-    }
+    gpio_set_output_options(GPIO_BANK_SPI1, GPIO_OTYPE_PP, GPIO_OSPEED_HIGH,
+                            GPIO_SPI1_SCK | GPIO_SPI1_MOSI);
+#elif STM32F1
+    gpio_set_mode(GPIO_BANK_SPI1, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                  GPIO_SPI1_SCK | GPIO_SPI1_MOSI);
+    gpio_set_mode(GPIO_BANK_SPI1, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT,
+                  GPIO_SPI1_MISO);
+  } else if (spi == SPI2) {
+    rcc_periph_clock_enable(RCC_SPI2);
+    rcc_periph_clock_enable(RCC_GPIOB);
+    // Configure GPIOs: SCK=PB13, MISO=PB14 and MOSI=PB15
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                  GPIO_SPI2_SCK | GPIO_SPI2_MOSI);
+    gpio_set_mode(GPIOB, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_SPI2_MISO);
+#endif
+  } else {
+    hacf();
+  }
 
-    // reset and shut down peripheral
-    spi_reset(spi);
+  // reset and shut down peripheral
+  spi_reset(spi);
 
-    // Set up SPI in Master mode with:
-    // - Clock baud rate: 1/64 of peripheral clock frequency
-    // - Clock polarity: Idle Low
-    // - Clock phase: Data valid on leading edge
-    // - Data frame format: 8-bit
-    // - Frame format: MSB First
-    spi_init_master(spi,
-                    SPI_CR1_BAUDRATE_FPCLK_DIV_64,
-                    SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
-                    SPI_CR1_CPHA_CLK_TRANSITION_1,
-                    SPI_CR1_DFF_8BIT,
-                    SPI_CR1_MSBFIRST
-    );
+  // Set up SPI in Master mode with:
+  // - Clock baud rate: 1/64 of peripheral clock frequency
+  // - Clock polarity: Idle Low
+  // - Clock phase: Data valid on leading edge
+  // - Data frame format: 8-bit
+  // - Frame format: MSB First
+#ifdef STM32F0
+  spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_64,
+                  SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+                  SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_MSBFIRST);
+  spi_set_data_size(SPI1, SPI_CR2_DS_8BIT);
+  spi_fifo_reception_threshold_8bit(SPI1);
+#elif STM32F1
+  spi_init_master(
+      spi, SPI_CR1_BAUDRATE_FPCLK_DIV_64, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+      SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
+#endif
 
-    spi_enable_software_slave_management(spi);
-    spi_set_nss_high(spi);
-
-    spi_enable(spi);
-    spi_nss(spi, true);
+  spi_enable(spi);
 }
 
-void
-spi_nss(uint32_t spi, bool new_state) {
-    void (*ptr)(uint32_t, uint16_t);
-
-    if (new_state) {
-        ptr = &gpio_set;
-    } else {
-        ptr = &gpio_clear;
-    }
-
-    // TODO(ppkt): SS pin / bank should be bound to device, not peripheral!
-    if (spi == SPI1) {
-        ptr(GPIO_BANK_SPI1_NSS, GPIO_SPI1_NSS);
-    } else if (spi == SPI2) {
-        ptr(GPIO_BANK_SPI2_NSS, GPIO_SPI2_NSS);
-    } else {
-        hacf();
-    }
-}
-
-void
-spi_send_transaction(uint32_t spi, uint8_t *tx, uint8_t *rx, uint8_t size) {
-    // TODO(ppkt): Add receiving data from SPI!
-    (void)rx;
-    spi_nss(spi, false);
-    for (uint8_t i = 0; i < size; ++i) {
-        spi_send(spi, tx[i]);
-        SPI_WAIT_FOR_TX_EMPTY(spi);
-        SPI_WAIT_FOR_BUSY_CLEAR(spi);
-    }
-    spi_nss(spi, true);
-
+void spi_send_transaction(uint32_t spi, const uint8_t *tx, uint8_t *rx,
+                          uint8_t size) {
+  for (uint8_t i = 0; i < size; ++i) {
+    spi_send(spi, tx[i]);
+    SPI_WAIT_FOR_TX_EMPTY(spi);
+    SPI_WAIT_FOR_RX_NOT_EMPTY(spi);
+    SPI_WAIT_FOR_BUSY_CLEAR(spi);
+    rx[i] = spi_read(spi);
+  }
 }
--- spi.h
+++ spi.h
@@ -6,30 +6,32 @@
 
 #include "utils.h"
 
+#define GPIO_BANK_SPI1 GPIOA
+#ifdef STM32F0
+#define GPIO_SPI1_SCK GPIO5
+#define GPIO_SPI1_MISO GPIO6
+#define GPIO_SPI1_MOSI GPIO7
+#endif
+
 #define SPI_WAIT_FOR_TX_EMPTY(spi) while (!(SPI_SR(spi) & SPI_SR_TXE))
-#define SPI_WAIT_FOR_BUSY_CLEAR(spi) while ((SPI_SR(spi) & SPI_SR_BSY));
+#define SPI_WAIT_FOR_RX_NOT_EMPTY(spi) while (!(SPI_SR(spi) & SPI_SR_RXNE))
+#define SPI_WAIT_FOR_BUSY_CLEAR(spi) while ((SPI_SR(spi) & SPI_SR_BSY))
 
 typedef struct spi_device {
-    uint32_t spi;
+  uint32_t spi;
 } spi_device;
 
+/**
+ * Initialize SPI peripheral in master mode (including RCC and GPIOs).
+ * Note: This function does not initialize NSS GPIO.
+ */
+void spi_init(uint32_t spi);
 
 /**
- * Initialize SPI peripheral (including RCC and GPIOs)
+ * Send / receive data to SPI slave in bulk.
+ * WARNING! Because it's possible that there could be more than one device on
+ * SPI bus, it's required to manually pull NSS pin down before calling this
+ * function (and pulling it up afterwards).
  */
-void
-spi_init(uint32_t spi);
-
-/**
- * Send / receive data to SPI slave using transaction (i.e. pull down SS before
- * 1st byte and pull up after all data is sent).
- */
-void
-spi_send_transaction(uint32_t spi, uint8_t *tx, uint8_t *rx, uint8_t size);
-
-/**
- * Set new value for SS depending on selected peripheral
- */
-void
-spi_nss(uint32_t spi, bool new_state);
-
+void spi_send_transaction(uint32_t spi, const uint8_t *tx, uint8_t *rx,
+                          uint8_t size);
