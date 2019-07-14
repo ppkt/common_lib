37499da35b4312da059bd9c077ae81a30713bf54
Karol Werner <karol@ppkt.eu>
2019-07-14 18:41:15 +0200

    Usart: Add macros for debugging purposes

--- usart.h
+++ usart.h
@@ -5,6 +5,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "utils.h"
 
@@ -25,3 +26,31 @@ void usart_printf(uint32_t usart, const char *format, ...);
   usart_printf(USART1, "%s = 0x%.2x\r\n", #variable, variable)
 #define print_variable_float(variable, precision)                              \
   usart_printf(USART1, "%s = %.*f\r\n", #variable, precision, variable)
+
+#define __fmt__ "%-5s:%-3d%10s(): "
+#define __FILENAME__ strrchr("/" __FILE__, '/') + 1
+#define trace()                                                                \
+  do {                                                                         \
+    if (DEBUG)                                                                 \
+      usart1_printf(__fmt__ "TRACE\n\r", __FILENAME__, __LINE__,               \
+                    __FUNCTION__);                                             \
+  } while (0)
+#define debug_print(string)                                                    \
+  do {                                                                         \
+    if (DEBUG)                                                                 \
+      usart1_printf(__fmt__ "%s", __FILENAME__, __LINE__, __FUNCTION__,        \
+                    string);                                                   \
+  } while (0)
+#define debug_printf(fmt, ...)                                                 \
+  do {                                                                         \
+    if (DEBUG)                                                                 \
+      usart1_printf(__fmt__ fmt, __FILENAME__, __LINE__, __FUNCTION__,         \
+                    __VA_ARGS__);                                              \
+  } while (0)
+
+#define hello_world(app_name)                                                  \
+  do {                                                                         \
+    if (DEBUG)                                                                 \
+      usart1_printf("[%s]\r\nCompiled: %s %s\r\n", app_name, __DATE__,         \
+                    __TIME__);                                                 \
+  } while (0)
